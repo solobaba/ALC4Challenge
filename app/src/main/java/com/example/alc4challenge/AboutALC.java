9c3c81e23579dd9ce6d4638b71f6a9f9972fd470
Solomon Oduniyi <solobaba_odu@yahoo.com>
2019-07-13 01:50:32 -0700

    ALC$Challenge

---
+++ app/src/main/java/com/example/alc4challenge/AboutALC.java
@@ -0,0 +1,35 @@
+package com.example.alc4challenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class AboutALC extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_alc);
+
+        //String url = getIntent().getStringExtra("url");
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://andela.com/alc/");
+        //webView.loadUrl(url);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/alc4challenge/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.alc4challenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button mAlc;
+    Button myProfile;
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAlc = findViewById(R.id.aboutalc);
+        myProfile = findViewById(R.id.myprofile);
+
+        myProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToMyProfile();
+            }
+        });
+
+        mAlc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(getApplicationContext(), AboutALC.class);
+//
+//                switch(view.getId())
+//                {
+//                    case R.id.action_bar:
+//                        intent.putExtra("url", "http://techcrunch.com/tag/rss/");
+//                        startActivity(intent);
+//                        break;
+//
+//                    default:
+//                        break;
+//                }
+
+                goToAlcAbout();
+            }
+        });
+    }
+
+    private void goToAlcAbout() {
+        startActivity( new Intent(MainActivity.this, AboutALC.class));
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://andela.com/alc/"));
+        startActivity(browserIntent);
+    }
+
+    private void goToMyProfile() {
+        startActivity( new Intent(MainActivity.this, MyProfile.class));
+    }
+}
